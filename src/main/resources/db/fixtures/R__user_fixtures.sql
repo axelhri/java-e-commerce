e31cb7c6f6d77b94e99d6c128f95298cf8bcc90c
Axelhri <axelhouairi@gmail.com>
2025-10-06 16:54:43 +0200

    feat(user): added user migration

--- src/main/resources/db/fixtures/R__user_fixtures.sql
+++ src/main/resources/db/fixtures/R__user_fixtures.sql
@@ -1,1 +1,9 @@
-INSERT INTO "user" (username, email, password) VALUES ('axel', 'axel@mail.com', 'secret');
+INSERT INTO users (email, password) VALUES
+    ('user@example.com', '$2a$10$w7ty1skmOPy.5T5PZ1dK5ePoqcWq1bZr4N7DlD/J.0AibFtB7DC8G'),
+    ('admin@example.com', '$2a$10$w7ty1skmOPy.5T5PZ1dK5ePoqcWq1bZr4N7DlD/J.0AibFtB7DC8G');
+
+INSERT INTO user_roles (user_id, role)
+SELECT id, 'USER' FROM users;
+
+INSERT INTO user_roles (user_id, role)
+SELECT id, 'ADMIN' FROM users WHERE email = 'admin@example.com';
--- src/main/resources/db/migration/V1__add_user_table.sql
+++ src/main/resources/db/migration/V1__add_user_table.sql
@@ -1,8 +1,17 @@
-CREATE TABLE "user" (
+CREATE TABLE users (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    username VARCHAR(15) NOT NULL UNIQUE,
-    email VARCHAR(250) NOT NULL UNIQUE,
-    password VARCHAR(250) NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    points INT NOT NULL DEFAULT 10000,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP
 );
+
+CREATE TABLE user_roles (
+    user_id UUID NOT NULL,
+    role VARCHAR(50) NOT NULL,
+    PRIMARY KEY (user_id, role),
+    FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE CASCADE
+);
+
+CREATE INDEX idx_user_roles_user_id ON user_roles(user_id);
