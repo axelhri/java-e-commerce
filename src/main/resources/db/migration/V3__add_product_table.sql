b5acf9f1a93873ebc97e1269682bcdef3e0f22b1
Axelhri <axelhouairi@gmail.com>
2025-10-07 15:48:50 +0200

    fix: fixed migration version

--- src/main/resources/db/migration/V3__add_product_table.sql
+++ src/main/resources/db/migration/V3__add_product_table.sql
@@ -1,9 +1,11 @@
 CREATE TABLE products (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     name VARCHAR(255) NOT NULL,
-    price Integer NOT NULL,
+    price INTEGER NOT NULL,
     description TEXT NOT NULL,
+    vendor_id UUID NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP
+    updated_at TIMESTAMP,
+    FOREIGN KEY (vendor_id) REFERENCES vendors(id) ON DELETE CASCADE
 );
 
