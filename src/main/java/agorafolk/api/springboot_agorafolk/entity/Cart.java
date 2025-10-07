11cdd10c5a09e3cb2a4fee4713f808d88c63668d
Axelhri <axelhouairi@gmail.com>
2025-10-07 10:49:31 +0200

    feat(cart): cart entity

---
+++ src/main/java/agorafolk/api/springboot_agorafolk/entity/Cart.java
@@ -0,0 +1,35 @@
+package agorafolk.api.springboot_agorafolk.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "carts")
+public class Cart {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
+  @Column(updatable = false, nullable = false, columnDefinition = "UUID")
+  private UUID id;
+
+  @Builder.Default
+  @Column(nullable = false)
+  private Integer total = 0;
+
+  @UpdateTimestamp
+  @Column(nullable = true)
+  private Instant updatedAt;
+
+  @OneToOne
+  @JoinColumn(name = "user_id", nullable = false, unique = true)
+  private User user;
+}
