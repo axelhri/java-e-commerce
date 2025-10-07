0fdc3b85d91f3d5477db15a0e1856ea06ca6ec2d
Axelhri <axelhouairi@gmail.com>
2025-10-07 16:40:15 +0200

    feat(product): product rating migration

---
+++ src/main/resources/db/migration/V9__add_product_rating_table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE products_ratings (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    product_id UUID NOT NULL REFERENCES products(id) ON DELETE CASCADE,
+    user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    rating INTEGER NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
