5af2e6003bd975c6459d40e2811893f4c9740d95
Axelhri <axelhouairi@gmail.com>
2025-11-05 17:59:57 +0100

    test(AuthenticationController): register successfully & should throw exception

---
+++ src/test/java/agorafolk/api/springboot_agorafolk/unit/controller/AuthenticationControllerUnitTest.java
@@ -0,0 +1,72 @@
+package agorafolk.api.springboot_agorafolk.unit.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import agorafolk.api.springboot_agorafolk.controller.AuthenticationController;
+import agorafolk.api.springboot_agorafolk.dto.AuthenticationRequest;
+import agorafolk.api.springboot_agorafolk.dto.AuthenticationResponse;
+import agorafolk.api.springboot_agorafolk.exception.UserAlreadyExistsException;
+import agorafolk.api.springboot_agorafolk.interfaces.AuthenticationServiceInterface;
+import java.util.UUID;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+@ExtendWith(MockitoExtension.class)
+class AuthenticationControllerUnitTest {
+  @Mock private AuthenticationServiceInterface authenticationService;
+
+  @InjectMocks private AuthenticationController authenticationController;
+
+  private AuthenticationRequest authRequest;
+
+  @BeforeEach
+  void setUp() {
+    authRequest = new AuthenticationRequest("test@mail.com", "Password123!");
+  }
+
+  @Nested
+  class registerUnitTest {
+    @Test
+    void registerShouldReturnSuccessResponseIfAccountIsCreated() {
+      // Arrange
+      AuthenticationResponse authResponse =
+          new AuthenticationResponse("jwtAccessToken", UUID.randomUUID());
+      when(authenticationService.register(authRequest)).thenReturn(authResponse);
+
+      // Act
+      ResponseEntity<AuthenticationResponse> response =
+          authenticationController.register(authRequest);
+
+      // Assert
+      verify(authenticationService).register(authRequest);
+
+      assertNotNull(response);
+      assertEquals(HttpStatus.CREATED, response.getStatusCode());
+      assertNotNull(response.getBody());
+      assertEquals("jwtAccessToken", response.getBody().accessToken());
+      assertEquals(authResponse.id(), response.getBody().id());
+    }
+
+    @Test
+    void registerShouldThrowUserAlreadyExistsException() {
+      // Arrange
+      when(authenticationService.register(authRequest))
+          .thenThrow(
+              new UserAlreadyExistsException(authRequest.email() + " is already registered"));
+
+      // Act & Assert
+      assertThrows(
+          UserAlreadyExistsException.class, () -> authenticationController.register(authRequest));
+
+      verify(authenticationService).register(authRequest);
+    }
+  }
+}
