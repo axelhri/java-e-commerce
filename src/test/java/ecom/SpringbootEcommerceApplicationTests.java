e9285f9eae3ed8d06dd243e6d907b56de4007122
Axelhri <axelhouairi@gmail.com>
2025-11-07 15:19:48 +0100

    refactor: test container abstract class

--- src/test/java/ecom/SpringbootEcommerceApplicationTests.java
+++ src/test/java/ecom/SpringbootEcommerceApplicationTests.java
@@ -2,29 +2,9 @@ package ecom;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
-import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
 
-@Testcontainers
 @SpringBootTest
 class SpringbootEcommerceApplicationTests {
-
-  @Container
-  static PostgreSQLContainer<?> postgres =
-      new PostgreSQLContainer<>("postgres:17")
-          .withDatabaseName("test")
-          .withUsername("test")
-          .withPassword("secret");
-
-  @DynamicPropertySource
-  static void configureDatasource(DynamicPropertyRegistry registry) {
-    registry.add("spring.datasource.url", postgres::getJdbcUrl);
-    registry.add("spring.datasource.username", postgres::getUsername);
-    registry.add("spring.datasource.password", postgres::getPassword);
-  }
 
   @Test
   void contextLoads() {
