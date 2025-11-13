551bbca5c6a1ba4c1cc85489f932db7d69fd66e7
Axelhri <axelhouairi@gmail.com>
2025-11-13 14:59:36 +0100

    feat(category): category entity created

---
+++ src/main/java/ecom/entity/Category.java
@@ -0,0 +1,43 @@
+package ecom.entity;
+
+import jakarta.persistence.*;
+import java.time.Instant;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+@Getter
+@Setter
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "categories")
+public class Category {
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
+  @Column(updatable = false, nullable = false, columnDefinition = "UUID")
+  private UUID id;
+
+  @Column(length = 50, nullable = false, unique = true)
+  private String name;
+
+  @CreationTimestamp
+  @Column(nullable = false, updatable = false)
+  private Instant createdAt;
+
+  @UpdateTimestamp @Column private Instant updatedAt;
+
+  @ManyToMany(fetch = FetchType.LAZY)
+  @JoinTable(
+      name = "category_parent",
+      joinColumns = @JoinColumn(name = "child_id"),
+      inverseJoinColumns = @JoinColumn(name = "parent_id"))
+  private Set<Category> parentCategory = new HashSet<>();
+
+  @OneToMany(mappedBy = "category", fetch = FetchType.LAZY)
+  private Set<Product> products = new HashSet<>();
+}
