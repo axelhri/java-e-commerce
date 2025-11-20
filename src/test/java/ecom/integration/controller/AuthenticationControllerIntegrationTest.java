3641238a196bb70643e40b3676820f94e8036221
Axelhri <axelhouairi@gmail.com>
2025-11-20 18:41:55 +0100

    test(CategoryController): create should return code 200

--- src/test/java/ecom/integration/controller/AuthenticationControllerIntegrationTest.java
+++ src/test/java/ecom/integration/controller/AuthenticationControllerIntegrationTest.java
@@ -17,9 +17,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest(properties = "spring.profiles.active=test")
@@ -30,7 +30,7 @@ public class AuthenticationControllerIntegrationTest extends PostgresTestContain
 
   @Autowired private ObjectMapper objectMapper;
 
-  @MockBean private AuthenticationServiceInterface authenticationService;
+  @MockitoBean private AuthenticationServiceInterface authenticationService;
 
   @Nested
   class registerIntegrationTest {
