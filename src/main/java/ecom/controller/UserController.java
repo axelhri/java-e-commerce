736a2ef3bd59aac452da430bdbf26074978d75be
Axelhri <axelhouairi@gmail.com>
2025-11-27 13:33:02 +0100

    refactor(user): changed controller response

--- src/main/java/ecom/controller/UserController.java
+++ src/main/java/ecom/controller/UserController.java
@@ -4,7 +4,9 @@ import ecom.dto.ApiResponse;
 import ecom.dto.ChangePassword;
 import ecom.interfaces.UserServiceInterface;
 import jakarta.validation.Valid;
+import java.time.Instant;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -17,9 +19,15 @@ public class UserController {
   private final UserServiceInterface userService;
 
   @PatchMapping("/password")
-  public ResponseEntity<ApiResponse> changePassword(
+  public ResponseEntity<ApiResponse<ChangePassword>> changePassword(
       @AuthenticationPrincipal UserDetails userDetails, @Valid @RequestBody ChangePassword dto) {
     userService.changePassword(userDetails.getUsername(), dto);
-    return ResponseEntity.ok(new ApiResponse(true, "Your password has been changed successfully"));
+    return ResponseEntity.status(HttpStatus.OK)
+        .body(
+            new ApiResponse<>(
+                Instant.now(),
+                HttpStatus.OK.value(),
+                "Your password has been changed successfully",
+                dto));
   }
 }
