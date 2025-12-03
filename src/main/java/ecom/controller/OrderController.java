8ddaf9c4f4082b11ebb367ddd8db2f3cc9f6ede1
Axelhri <axelhouairi@gmail.com>
2025-12-03 13:26:16 +0100

    feat(order): create order endpoint

---
+++ src/main/java/ecom/controller/OrderController.java
@@ -0,0 +1,31 @@
+package ecom.controller;
+
+import ecom.dto.ApiResponse;
+import ecom.dto.OrderRequest;
+import ecom.dto.OrderResponse;
+import ecom.entity.User;
+import ecom.interfaces.OrderServiceInterface;
+import jakarta.validation.Valid;
+import java.time.Instant;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/api/v1/orders")
+public class OrderController {
+  private final OrderServiceInterface orderService;
+
+  @PostMapping
+  public ResponseEntity<ApiResponse<OrderResponse>> addProductToCart(
+      @AuthenticationPrincipal User user, @Valid @RequestBody OrderRequest dto) {
+    OrderResponse response = orderService.createOrder(user, dto);
+    return ResponseEntity.status(HttpStatus.CREATED)
+        .body(
+            new ApiResponse<>(
+                Instant.now(), HttpStatus.CREATED.value(), "Order passed successfully", response));
+  }
+}
