d3788e122ee811e2d69b33275e9f86b85941dbcc
Axelhri <axelhouairi@gmail.com>
2025-12-15 20:40:25 +0100

    feat(order): get order by id endpoint

--- src/main/java/ecom/controller/OrderController.java
+++ src/main/java/ecom/controller/OrderController.java
@@ -9,6 +9,7 @@ import ecom.interfaces.OrderServiceInterface;
 import jakarta.validation.Valid;
 import java.time.Instant;
 import java.util.List;
+import java.util.UUID;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -48,5 +49,14 @@ public class OrderController {
     return ResponseEntity.ok(
         new ApiResponse<>(
             Instant.now(), HttpStatus.OK.value(), "Orders fetched successfully", orders));
+  }
+
+  @GetMapping("/{orderId}")
+  public ResponseEntity<ApiResponse<OrderResponse>> getOrderById(
+      @AuthenticationPrincipal User user, @PathVariable UUID orderId) {
+    OrderResponse order = orderService.getOrderById(user, orderId);
+    return ResponseEntity.ok(
+        new ApiResponse<>(
+            Instant.now(), HttpStatus.OK.value(), "Order fetched successfully", order));
   }
 }
