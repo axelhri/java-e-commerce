09149b1775b1e04a726650e72679a508cc6cd567
Axelhri <axelhouairi@gmail.com>
2025-12-16 15:46:56 +0100

    feat(product): get products endpoint

--- src/main/java/ecom/controller/ProductController.java
+++ src/main/java/ecom/controller/ProductController.java
@@ -2,16 +2,17 @@ package ecom.controller;
 
 import ecom.dto.ApiResponse;
 import ecom.dto.ProductRequest;
+import ecom.dto.ProductResponse;
 import ecom.interfaces.ProductServiceInterface;
 import jakarta.validation.Valid;
 import java.time.Instant;
+import java.util.UUID;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @AllArgsConstructor
@@ -26,7 +27,16 @@ public class ProductController {
     productService.createProduct(dto);
     return ResponseEntity.status(HttpStatus.CREATED)
         .body(
-            new ApiResponse<ProductRequest>(
+            new ApiResponse<>(
                 Instant.now(), HttpStatus.CREATED.value(), "Product created successfully", dto));
+  }
+
+  @GetMapping
+  public ResponseEntity<ApiResponse<Page<ProductResponse>>> getAllProducts(
+      @RequestParam(required = false) UUID categoryId, Pageable pageable) {
+    Page<ProductResponse> products = productService.getAllProducts(categoryId, pageable);
+    return ResponseEntity.ok(
+        new ApiResponse<>(
+            Instant.now(), HttpStatus.OK.value(), "Products fetched successfully", products));
   }
 }
