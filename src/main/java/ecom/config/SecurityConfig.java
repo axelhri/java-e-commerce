d28ee98f8bd510dbeab8861bd40b46fe934435e1
Axelhri <axelhouairi@gmail.com>
2025-12-16 15:48:11 +0100

    feat(security): get method public access for products endpoint

--- src/main/java/ecom/config/SecurityConfig.java
+++ src/main/java/ecom/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import ecom.model.Role;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -34,6 +35,8 @@ public class SecurityConfig {
                     .hasRole(Role.ADMIN.name())
                     .requestMatchers("/api/v1/categories")
                     .hasRole(Role.ADMIN.name())
+                    .requestMatchers(HttpMethod.GET, "/api/v1/products")
+                    .permitAll()
                     .requestMatchers("/api/v1/products")
                     .hasRole(Role.ADMIN.name())
                     .anyRequest()
