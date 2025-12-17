3b8a3418e811702440b2409015fec9095a8a1050
Axelhri <axelhouairi@gmail.com>
2025-12-17 23:25:54 +0100

    feat(rating): get vendor average review logic

--- src/main/java/ecom/interfaces/RatingServiceInterface.java
+++ src/main/java/ecom/interfaces/RatingServiceInterface.java
@@ -3,7 +3,10 @@ package ecom.interfaces;
 import ecom.dto.RatingRequest;
 import ecom.dto.RatingResponse;
 import ecom.entity.User;
+import java.util.UUID;
 
 public interface RatingServiceInterface {
   RatingResponse sendProductRating(User user, RatingRequest request);
+
+  Double getVendorRating(UUID vendorId);
 }
