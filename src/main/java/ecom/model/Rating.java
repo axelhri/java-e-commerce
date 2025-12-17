c2502b15d6282fd1aef23759dabec5bdf165667b
Axelhri <axelhouairi@gmail.com>
2025-12-17 18:52:11 +0100

    refactor(rating): refined fromValue method

--- src/main/java/ecom/model/Rating.java
+++ src/main/java/ecom/model/Rating.java
@@ -1,5 +1,8 @@
 package ecom.model;
 
+import java.util.Arrays;
+import java.util.Map;
+import java.util.stream.Collectors;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
@@ -13,12 +16,14 @@ public enum Rating {
 
   @Getter private final Integer rating;
 
+  private static final Map<Integer, Rating> RATING_MAP =
+      Arrays.stream(values()).collect(Collectors.toMap(Rating::getRating, r -> r));
+
   public static Rating fromValue(int value) {
-    for (Rating r : values()) {
-      if (r.rating == value) {
-        return r;
-      }
+    Rating rating = RATING_MAP.get(value);
+    if (rating == null) {
+      throw new IllegalArgumentException("Invalid rating value: " + value);
     }
-    throw new IllegalArgumentException("Invalid rating value: " + value);
+    return rating;
   }
 }
