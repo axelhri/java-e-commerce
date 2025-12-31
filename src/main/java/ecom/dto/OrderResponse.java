541a9cafc8548936b71144f9cab50ebc4af2ec8d
Axelhri <axelhouairi@gmail.com>
2025-12-31 09:37:07 +0100

    docs: order & cart records swagger documentation

--- src/main/java/ecom/dto/OrderResponse.java
+++ src/main/java/ecom/dto/OrderResponse.java
@@ -1,10 +1,21 @@
 package ecom.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
 import java.math.BigDecimal;
 import java.util.Set;
 import java.util.UUID;
 
 public record OrderResponse(
-    @JsonProperty("ordered_items") Set<UUID> productsIds,
-    @JsonProperty("order_price") BigDecimal price) {}
+    @Schema(
+            description = "Set of ordered product IDs",
+            example = "[\"123e4567-e89b-12d3-a456-426614174000\"]",
+            accessMode = Schema.AccessMode.READ_ONLY)
+        @JsonProperty("ordered_items")
+        Set<UUID> productsIds,
+    @Schema(
+            description = "Total price of the order",
+            example = "150.50",
+            accessMode = Schema.AccessMode.READ_ONLY)
+        @JsonProperty("order_price")
+        BigDecimal price) {}
