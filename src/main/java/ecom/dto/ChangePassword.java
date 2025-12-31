8b057695bc0d99de468966db5fae9774f07bbd67
Axelhri <axelhouairi@gmail.com>
2025-12-31 09:35:06 +0100

    docs: authentication records swagger documentation

--- src/main/java/ecom/dto/ChangePassword.java
+++ src/main/java/ecom/dto/ChangePassword.java
@@ -2,17 +2,28 @@ package ecom.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import ecom.interfaces.PasswordMatch;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 @PasswordMatch
 public record ChangePassword(
-    @JsonProperty("current_password")
+    @Schema(
+            description = "Current user password",
+            example = "OldPassword123!",
+            minLength = 8,
+            format = "password")
+        @JsonProperty("current_password")
         @NotBlank(message = "Current password is required")
         @Size(min = 8, message = "Current password must be at least 8 characters long")
         String currentPassword,
-    @JsonProperty("new_password")
+    @Schema(
+            description = "New password",
+            example = "NewPassword123!",
+            minLength = 8,
+            format = "password")
+        @JsonProperty("new_password")
         @NotBlank(message = "New password is required")
         @Size(min = 8, message = "New password must be at least 8 characters long")
         @Pattern(
@@ -20,5 +31,11 @@ public record ChangePassword(
             message =
                 "Password must contain at least one uppercase letter, one digit, and one special character.")
         String newPassword,
-    @JsonProperty("confirm_password") @NotBlank(message = "You must confirm your password")
+    @Schema(
+            description = "Confirmation of the new password (must match new_password)",
+            example = "NewPassword123!",
+            minLength = 8,
+            format = "password")
+        @JsonProperty("confirm_password")
+        @NotBlank(message = "You must confirm your password")
         String confirmPassword) {}
