dc54e336f96812788a325556eb1b7eea50c8087b
Axelhri <axelhouairi@gmail.com>
2026-01-03 12:50:32 +0100

    refactor(cart): findAllByCartUser query

--- src/main/java/neora/repository/CartItemRepository.java
+++ src/main/java/neora/repository/CartItemRepository.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 import neora.entity.CartItem;
+import neora.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -14,4 +15,6 @@ public interface CartItemRepository extends JpaRepository<CartItem, UUID> {
   List<CartItem> findByCartId(UUID cartId);
 
   void deleteByCartId(UUID cartId);
+
+  List<CartItem> findAllByCartUser(User user);
 }
