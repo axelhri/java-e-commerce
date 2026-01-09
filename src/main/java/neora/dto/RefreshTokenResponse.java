898b9ccda95af615ed5cec6ffd024ad7b60d65f1
Axelhri <axelhouairi@gmail.com>
2026-01-09 15:21:23 +0100

    refactor(RefreshTokenResponse): ignore tokens in json

--- src/main/java/neora/dto/RefreshTokenResponse.java
+++ src/main/java/neora/dto/RefreshTokenResponse.java
@@ -1,5 +1,6 @@
 package neora.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.v3.oas.annotations.media.Schema;
 import java.util.UUID;
@@ -9,17 +10,17 @@ public record RefreshTokenResponse(
             description = "New JWT Access Token",
             example = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...",
             accessMode = Schema.AccessMode.READ_ONLY)
-        @JsonProperty("access_token")
+        @JsonIgnore
         String accessToken,
     @Schema(
             description = "Refresh Token used to obtain new access token",
             example = "d9b2d63d-a233-4123-85f8-77654321e321",
             accessMode = Schema.AccessMode.READ_ONLY)
-        @JsonProperty("refresh_token")
+        @JsonIgnore
         String refreshToken,
     @Schema(
             description = "User unique identifier",
             example = "123e4567-e89b-12d3-a456-426614174000",
             accessMode = Schema.AccessMode.READ_ONLY)
         @JsonProperty("user_id")
-        UUID id) {}
+        UUID userId) {}
