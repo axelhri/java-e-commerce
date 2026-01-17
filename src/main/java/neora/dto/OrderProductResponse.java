6c3fc5be40930fdb457673f544878ea4e878cd1d
Axelhri <axelhouairi@gmail.com>
2026-01-17 17:01:21 +0100

    feat(order): record open api doc

--- src/main/java/neora/dto/OrderProductResponse.java
+++ src/main/java/neora/dto/OrderProductResponse.java
@@ -1,3 +1,24 @@
 package neora.dto;
 
-public record OrderProductResponse(String productId, String image, Integer quantity) {}
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
+
+public record OrderProductResponse(
+    @Schema(
+            description = "Product unique identifier",
+            example = "123e4567-e89b-12d3-a456-426614174000",
+            accessMode = Schema.AccessMode.READ_ONLY)
+        @JsonProperty("product_id")
+        String productId,
+    @Schema(
+            description = "Product primary image URL",
+            example = "http://res.cloudinary.com/...",
+            accessMode = Schema.AccessMode.READ_ONLY)
+        @JsonProperty("product_image")
+        String image,
+    @Schema(
+            description = "Product quantity in order",
+            example = "3",
+            accessMode = Schema.AccessMode.READ_ONLY)
+        @JsonProperty("quantity")
+        Integer quantity) {}
