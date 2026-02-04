861b2732d6fa61b0a07fa4fba4fa9f39e739ba16
Axelhri <axelhouairi@gmail.com>
2026-02-04 13:39:28 +0100

    feat(bookmarks): service logs

--- src/main/java/neora/service/BookmarkService.java
+++ src/main/java/neora/service/BookmarkService.java
@@ -1,6 +1,7 @@
 package neora.service;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import neora.dto.BookmarkResponse;
 import neora.dto.ManageBookmarkRequest;
 import neora.entity.Bookmark;
@@ -17,6 +18,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 @AllArgsConstructor
+@Slf4j
 public class BookmarkService implements BookmarkServiceInterface {
   private final UserRepository userRepository;
   private final BookmarkRepository bookmarkRepository;
@@ -25,22 +27,34 @@ public class BookmarkService implements BookmarkServiceInterface {
 
   @Override
   public BookmarkResponse bookmarkProduct(ManageBookmarkRequest request, User user) {
+    log.info(
+        "Attempting to bookmark product ID: {} for user ID: {}", request.productId(), user.getId());
+
     if (!userRepository.existsById(user.getId())) {
+      log.error("User not found for ID: {}", user.getId());
       throw new ResourceNotFoundException("User not found");
     }
 
     Product product =
         productRepository
             .findById(request.productId())
-            .orElseThrow(() -> new ResourceNotFoundException("Product not found"));
+            .orElseThrow(
+                () -> {
+                  log.error("Product not found for ID: {}", request.productId());
+                  return new ResourceNotFoundException("Product not found");
+                });
 
     if (bookmarkRepository.findByUserAndProduct(user, product).isPresent()) {
+      log.warn(
+          "Product ID: {} is already bookmarked by user ID: {}", product.getId(), user.getId());
       throw new ResourceAlreadyExistsException("Product already bookmarked");
     }
 
     Bookmark bookmark = bookmarkMapper.toBookmarkEntity(product, user);
 
     bookmarkRepository.save(bookmark);
+    log.info(
+        "Product ID: {} successfully bookmarked for user ID: {}", product.getId(), user.getId());
 
     return new BookmarkResponse(
         bookmark.getId(), bookmark.getProduct().getId(), bookmark.getUser().getId());
@@ -48,21 +62,41 @@ public class BookmarkService implements BookmarkServiceInterface {
 
   @Override
   public void removeProductFromBookmarks(ManageBookmarkRequest request, User user) {
+    log.info(
+        "Attempting to remove bookmark for product ID: {} and user ID: {}",
+        request.productId(),
+        user.getId());
 
     if (!userRepository.existsById(user.getId())) {
+      log.error("User not found for ID: {}", user.getId());
       throw new ResourceNotFoundException("User not found");
     }
 
     Product product =
         productRepository
             .findById(request.productId())
-            .orElseThrow(() -> new ResourceNotFoundException("Product not found"));
+            .orElseThrow(
+                () -> {
+                  log.error("Product not found for ID: {}", request.productId());
+                  return new ResourceNotFoundException("Product not found");
+                });
 
     Bookmark bookmark =
         bookmarkRepository
             .findByUserAndProduct(user, product)
-            .orElseThrow(() -> new ResourceNotFoundException("Product not found in bookmarks"));
+            .orElseThrow(
+                () -> {
+                  log.warn(
+                      "Bookmark not found for user ID: {} and product ID: {}",
+                      user.getId(),
+                      product.getId());
+                  return new ResourceNotFoundException("Product not found in bookmarks");
+                });
 
     bookmarkRepository.delete(bookmark);
+    log.info(
+        "Bookmark removed successfully for product ID: {} and user ID: {}",
+        product.getId(),
+        user.getId());
   }
 }
